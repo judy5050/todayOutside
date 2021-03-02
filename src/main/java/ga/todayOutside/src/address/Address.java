20be8e1ce08d0e307ef9d7db81d3ae68af61c728
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-03-02 20:39:22 +0900

    3시간 단위로 날씨 받아오기완료,회원 주소값 받는 코드 작성중

---
+++ src/main/java/ga/todayOutside/src/address/Address.java
@@ -0,0 +1,55 @@
+package ga.todayOutside.src.address;
+
+import ga.todayOutside.config.BaseEntity;
+import ga.todayOutside.src.user.models.UserInfo;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="address")
+public class Address  extends BaseEntity {
+
+    /**
+     * 주소 인덱스
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="addressIdx")
+    private Long id;
+
+    /**
+     *도  ex 경기도 ..
+     */
+    @Column(name = "firstAddressName")
+    private String firstAddressName;
+
+    /**
+     * 시
+     */
+    @Column(name = "secondAddressName")
+    private String secondAddressName;
+
+    /**
+     * 구
+     */
+    @Column(name="thirdAddressName")
+    private String thirdAddressName;
+
+
+
+
+    /**
+     * 회원엔티티
+     */
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn("userIdx")
+//    private UserInfo userInfo;
+
+
+
+}
