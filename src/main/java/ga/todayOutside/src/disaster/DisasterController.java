34520c0ef584b454a11d4fd2b0ca1ed0229dcdab
rheew <47098896+rheew@users.noreply.github.com>
2021-03-09 17:58:04 +0900

    필터 구현중, 필터 메소드 구현 필요

--- src/main/java/ga/todayOutside/src/disaster/DisasterController.java
+++ src/main/java/ga/todayOutside/src/disaster/DisasterController.java
@@ -1,12 +1,16 @@
 package ga.todayOutside.src.disaster;
 
-import ga.todayOutside.src.user.UserInfoController;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,10 +29,44 @@ public class DisasterController {
      */
     @ResponseBody
     @GetMapping("/info")
-    public Map<String, Object> getInfomation() {
-        Map<String, Object> result = new HashMap<String, Object>();
+    public Map<String, Object> getInfomation() throws ParseException {
+        Map<String, Object> result = disasterService.getImfomation();
+        Map<Object, ArrayList<Map<String, JSONObject>>> fileter = new HashMap<>();
 
-        result = disasterService.getImfomation();
+        Integer status = (Integer) result.get("status");
+        String jsonString = (String) result.get("body");
+
+        // string json 형식으로 들어옴
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(jsonString);
+        JSONObject jsonObj = (JSONObject) obj;
+        JSONArray disasterMsg = (JSONArray) jsonObj.get("DisasterMsg");
+        JSONObject row = (JSONObject) disasterMsg.get(1);
+        JSONArray messages = (JSONArray) row.get("row");
+
+        for (Object o : messages) {
+            Map<String, JSONObject> msgs = new HashMap<>();
+
+            JSONObject jo = (JSONObject) o;
+            String locationName = (String) jo.get("location_name");
+            String location[] = locationName.split(" ");
+            String state = location[0];
+            String city = location[1];
+
+            ArrayList<Map<String, JSONObject>> regCity = fileter.getOrDefault("state", new ArrayList<Map<String, JSONObject>>());
+            regCity.add(msgs.put(city, jo));
+            fileter.put(state, regCity);
+
+        }
+
+        /**
+         출력 결과 {경기도=[null]}
+         문제점 null 값으로 value가 입력 된다.
+         지역에 다른 필터 메소드를 따로 만드는 것이 좋을 것 같음
+
+         */
+        System.out.println(fileter);
         return result;
     }
 }
--- src/main/java/ga/todayOutside/src/disaster/DisasterService.java
+++ src/main/java/ga/todayOutside/src/disaster/DisasterService.java
@@ -4,17 +4,16 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
 
-import java.io.Reader;
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,40 +27,40 @@ public class DisasterService {
         HashMap<String, Object> result = new HashMap<>();
 
         try {
-            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-            factory.setConnectTimeout(5000);
-            factory.setReadTimeout(5000);
-            RestTemplate restTemplate = new RestTemplate(factory);
+            String url = "http://apis.data.go.kr";
+            //uricomponentbuilder를 내부적으로 사용해 uri 템플릿을 확장하고 인코딩
+            DefaultUriBuilderFactory  factory = new DefaultUriBuilderFactory(url);
 
-            HttpHeaders headers = new HttpHeaders();
-            HttpEntity entity = new HttpEntity<>(headers);
-            headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+            RestTemplate restTemplate = new RestTemplateBuilder().build();
+            factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
 
-            String url = "http://apis.data.go.kr/1741000/DisasterMsg2/getDisasterMsgList";
             String serviceKey = "BtXq5fRG2%2Bv%2B%2FEVKm3iuwIj%2BjPQnTRsO3yp6ZhtElvdMODumC6aSKDBkKtamNx9yp6YDVxes2fz5bK5FxZJI1Q%3D%3D";
             String pageNo = "1";
-            String numOfRows = "10";
+            String numOfRows = "3";
             String type = "json";
             String flag = "y";
 
-            UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+            UriBuilder uriBuilder = factory.builder();
+
+            uriBuilder.path("/1741000/DisasterMsg2/getDisasterMsgList")
                     .queryParam("ServiceKey", serviceKey)
                     .queryParam("pageNo", pageNo)
                     .queryParam("numOfRows", numOfRows)
                     .queryParam("type", type)
-                    .queryParam("flag", flag)
-                    .build();
-            System.out.println(builder.toString());
+                    .queryParam("flag", flag);
 
-            ResponseEntity response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
 
-            JSONParser parser = new JSONParser();
-            JSONObject obj = (JSONObject) parser.parse(response.toString());
+            ResponseEntity response = restTemplate.exchange(uriBuilder.build(), HttpMethod.GET, null, String.class);
 
-            System.out.println(response);
-        } catch (HttpClientErrorException | HttpServerErrorException | ParseException e) {
-            return result;
+            result.put("body", response.getBody());
+            result.put("status", response.getStatusCodeValue());
+
+
+        } catch (HttpClientErrorException | HttpServerErrorException   e) {
+            result.put("statusCode", e.getRawStatusCode());
+
         }
+        System.out.println(result);
         return result;
     }
 
