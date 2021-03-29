456915a458e51017efe9a0f42128cbd1a1eee923
rheew <47098896+rheew@users.noreply.github.com>
2021-03-29 21:05:15 +0900

    Disaster model 추가

---
+++ src/main/java/ga/todayOutside/src/disaster/model/DisasterAlarmUser.java
@@ -0,0 +1,13 @@
+package ga.todayOutside.src.disaster.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class DisasterAlarmUser {
+    private List<String> alarmKinds;
+    private String targetToken;
+}
